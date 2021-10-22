50a140ded09b97c1a8bcee1cad5cfde5a0557876
Felipe <felipecostasdc@gmail.com>
2021-10-22 10:07:32 -0300

    cores dinamicas de acordo com o tipo de pokemon

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,20 +1,23 @@
-import { Li, PokeNumber, Kind } from './styles';
+import { Li, PokemonId, Kind } from './styles';
+import Colors from '../../resources/colors';
 
 function Pokemon({pokemon}) {
     // nome com a primeira letra maiuscula
     let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
     let kinds = pokemon.kind.split(';')
+    let colorKind = Colors[kinds[0]]
+    let colorKindLight = colorKind.concat("AA") 
+    console.log(colorKind)
+
     return (
-      <Li>
-        <PokeNumber># {pokemon.id}</PokeNumber>
-        <strong>{name}</strong>
-        <img src={pokemon.image_url}/>
+      <Li background={Colors.pokemonBackground} hoverColor={colorKindLight}>
+        <PokemonId color={colorKind}># {pokemon.id}</PokemonId>
+        <strong style={{fontSize: '1.1em'}}>{name}</strong>
+        <img class='pokemon-img' src={pokemon.image_url}/>
 
         <div style={{display: 'inline'}}>
-          {kinds.map(kind => <Kind color={"#BB10A0"}>{`${kind}`}</Kind>)}
+          {kinds.map(kind => <Kind color={Colors[kind]}>{`${kind}`}</Kind>)}
         </div>
-
-
       </Li>
     )
 }
--- src/components/Pokemon/styles.js
+++ src/components/Pokemon/styles.js
@@ -11,38 +11,57 @@ export const Li = styled.li`
     border-radius: 12px;
     list-style-type: none;
     text-align: start;
+
     user-select: none;
-    background-color: #1e1117;
-    color: white;
+    background-color: ${props => props.background};
+    color: black;
     cursor: pointer;
 
-    > img {
+    box-shadow: 1px 1px 8px lightgrey;
+
+    .pokemon-img {
         width: 200px;
         height: 200px;
     }
 
-    &:hover > img {
-        transition: .5s;
-        transform: scale(1.07);
+    &:hover {
+        color: white;
+        background-color: ${props => props.hoverColor};
+        
+        .pokemon-img {
+            transition: .5s;
+            transform: scale(1.07);
+            filter: drop-shadow(0px 0px 1px black)
+        }
+        
+        > strong {
+            text-shadow: 0px 0px 1px black;
+            transition: .3s;
+            transform: scale(1.2);
+        }
+
     }
 `
 
-export const PokeNumber = styled.span`
+export const PokemonId = styled.span`
     position: absolute;
     top: 0;
     right: 0;
     padding: 4px;
-    background-color: #E3350DEE;
+    color: white;
+    background-color: ${props => props.color};
     border-top-right-radius: 12px;
     border-bottom-left-radius: 12px;
-
 `
 
 
 export const Kind = styled.span`
     background-color: ${props => props.color};
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     border-radius: 5px;
     margin: 0.5em;
-    padding: 2px;
+    padding: 3px;
+    color: black;
     align-items: center;
+    font-weight: bold;
 `
